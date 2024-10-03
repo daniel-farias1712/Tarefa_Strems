80d00c87829b62ab95689c342ec494368c8fa00c
daniel-farias1712 <112397725+daniel-farias1712@users.noreply.github.com>
2024-10-03 13:47:34 -0300

    Add files via upload

---
+++ Atividade_Ebac/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+
+       
+        System.out.println("Escreva a idade e o sexo 5 vezes ou mais como no exemplo -- Idade,Sexo -- \\n'M' -Para macho\\nE 'F' Femeas");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Escreva a idade e o sexo (Idade,Sexo):");
+            String entrada = scan.nextLine();
+            String[] divisao = entrada.split(",");
+
+            if (divisao.length == 2) {
+                try {
+                    int idade = Integer.parseInt(divisao[0].trim());
+                    String sexo = divisao[1].trim();
+
+                    pessoas.add(new Pessoa(idade, sexo));
+                } catch (NumberFormatException e) {
+                    System.out.println("Erro: Idade deve ser um número.");
+                }
+            } else {
+                System.out.println("Entrada inválida. Por favor, siga o formato 'Idade,Sexo'.");
+            }
+        }
+
+
+        List<Pessoa> feminino = pessoas.stream()
+                .filter(p -> p.getSexo().equalsIgnoreCase("F"))
+                .collect(Collectors.toList());
+
+        List<Pessoa> masculino = pessoas.stream()
+                .filter(p -> p.getSexo().equalsIgnoreCase("M"))
+                .collect(Collectors.toList());
+
+
+        System.out.println("Total de pessoas cadastradas: " + pessoas.size());
+        System.out.println("Número de mulheres: " + feminino.size());
+        System.out.println("Número de homens: " + masculino.size());
+    }
+}
